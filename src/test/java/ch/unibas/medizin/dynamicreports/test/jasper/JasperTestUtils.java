c57707d3054e8380222a09aba09c038707196dd4
Martin Imobersteg <martin.imobersteg@gmail.com>
2026-03-06 08:06:28 +0100

    Fix formatting

--- src/test/java/ch/unibas/medizin/dynamicreports/test/jasper/JasperTestUtils.java
+++ src/test/java/ch/unibas/medizin/dynamicreports/test/jasper/JasperTestUtils.java
@@ -51,29 +51,20 @@ public class JasperTestUtils {
         String band;
         final DRSubtotal<?> subtl = subtotal.getSubtotal();
         switch (subtl.getPosition()) {
-  case TITLE -> 
-band = "title";
-  case PAGE_HEADER -> 
-band = "pageHeader";
-  case PAGE_FOOTER -> 
-band = "pageFooter";
-  case COLUMN_HEADER -> 
-band = "columnHeader";
-  case COLUMN_FOOTER -> 
-band = "columnFooter";
-  case GROUP_HEADER, FIRST_GROUP_HEADER, LAST_GROUP_HEADER -> 
-band = "subtotalGroupHeader";
-  case GROUP_FOOTER, FIRST_GROUP_FOOTER, LAST_GROUP_FOOTER -> 
-band = "subtotalGroupFooter";
-  case LAST_PAGE_FOOTER -> 
-band = "lastPageFooter";
-  case SUMMARY -> 
-band = "summary";
-  default -> {
-Assertions.fail("Subtotal position " + subtl.getPosition().name() + " not found");
+            case TITLE -> band = "title";
+            case PAGE_HEADER -> band = "pageHeader";
+            case PAGE_FOOTER -> band = "pageFooter";
+            case COLUMN_HEADER -> band = "columnHeader";
+            case COLUMN_FOOTER -> band = "columnFooter";
+            case GROUP_HEADER, FIRST_GROUP_HEADER, LAST_GROUP_HEADER -> band = "subtotalGroupHeader";
+            case GROUP_FOOTER, FIRST_GROUP_FOOTER, LAST_GROUP_FOOTER -> band = "subtotalGroupFooter";
+            case LAST_PAGE_FOOTER -> band = "lastPageFooter";
+            case SUMMARY -> band = "summary";
+            default -> {
+                Assertions.fail("Subtotal position " + subtl.getPosition().name() + " not found");
                 return null;
-}
-}
+            }
+        }
         return band + ".column_" + subtl.getShowInColumn().getName() + ".subtotal";
     }
 
