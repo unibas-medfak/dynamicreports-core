b02a9e6723966a588979e075a4c4f15fc620426b
bugix <martin.imobersteg@gmail.com>
2026-05-30 04:35:24 +0000

    Deploying to javadoc from @ unibas-medfak/dynamicreports-core@3793e86eb8d84bb9123418727f548243362aea2a 🚀

--- javadoc.sh
+++ javadoc.sh
@@ -1,1 +1,1 @@
-/opt/hostedtoolcache/Java_Adopt_jdk/25.0.2-10.0.LTS/x64/bin/javadoc -J-Duser.language= -J-Duser.country= @options @argfile
+/opt/hostedtoolcache/Java_Adopt_jdk/25.0.3-9.0.LTS/x64/bin/javadoc -J-Duser.language= -J-Duser.country= @options @argfile
